e626226bc7ada65e123e2fdddec68c2fc47429d7
Germano Gabbianelli <tyrion.mx@gmail.com>
2013-09-28 03:45:01 +0000

    added production settings

--- bakehouse/settings/production.py
+++ bakehouse/settings/production.py
@@ -3,15 +3,25 @@ from base import *
 # Normally you should not import ANYTHING from Django directly
 # into your settings, but ImproperlyConfigured is an exception.
 
+ALLOWED_HOSTS = ['bakehouse.tyrion.mx']
 
 ########## DATABASE CONFIGURATION
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dash',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
 ########## END DATABASE CONFIGURATION
 
 
 ########## CACHE CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#caches
-CACHES = {}
+# CACHES = {}
 ########## END CACHE CONFIGURATION
 
 
@@ -19,3 +29,16 @@ CACHES = {}
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = get_env_setting('SECRET_KEY')
 ########## END SECRET CONFIGURATION
+
+INSTALLED_APPS += ('storages',)
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+AWS_ACCESS_KEY_ID = get_env_setting('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = get_env_setting('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = 'static.tyrion.mx'
+AWS_QUERYSTRING_AUTH = False
+AWS_S3_SECURE_URLS = False
+AWS_S3_CUSTOM_DOMAIN = AWS_STORAGE_BUCKET_NAME
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+STATIC_ROOT = ''
+STATIC_URL = 'http://static.tyrion.mx/'
