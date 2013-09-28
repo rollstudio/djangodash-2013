cd1e73c1c55d6cd5e409721bea04b29794d7dda4
Patrick Arminio <patrick.arminio@gmail.com>
2013-09-28 02:53:49 +0200

    Enabled admin

--- bakehouse/settings/base.py
+++ bakehouse/settings/base.py
@@ -135,15 +135,12 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- bakehouse/urls.py
+++ bakehouse/urls.py
@@ -1,8 +1,7 @@
+from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 # TODO: remove
 from django.views.generic.base import TemplateView
@@ -17,9 +16,5 @@ urlpatterns = patterns('',
     url(r'^$', ComingView.as_view(), name='home'),
     # url(r'^bakehouse/', include('bakehouse.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
