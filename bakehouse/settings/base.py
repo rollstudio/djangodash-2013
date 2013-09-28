2b2550f70c07823ffd4760dcd2030fb803cf55b9
Patrick Arminio <patrick.arminio@gmail.com>
2013-09-28 03:41:03 +0200

    Added the right scope for github auth.

--- bakehouse/settings/base.py
+++ bakehouse/settings/base.py
@@ -161,6 +161,12 @@ INSTALLED_APPS = (
 
 # SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
 
+SOCIALACCOUNT_PROVIDERS = {
+    'github': {
+        'SCOPE': ['repo'],
+    }
+}
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
