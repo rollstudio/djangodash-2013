7c6090a763a2acbb09babda4ab9ef4e9b64cb308
Germano Gabbianelli <tyrion.mx@gmail.com>
2013-09-28 22:13:47 +0000

    Trying to make the views

--- cookiecutters/views.py
+++ cookiecutters/views.py
@@ -1,7 +1,24 @@
 from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404, render, redirect
 
 from .models import CookieCutter
 from .forms import GeneratorForm
+
+
+def cookiecutter_detail(request, username, cookie):
+    cookie = get_object_or_404(CookieCutter, user__username=username, name=cookie)
+    return render(request, 'cookiecutters/detail.html', {'cookie': cookie})
+
+
+def bake(request, username, cookie):
+    cookie = get_object_or_404(CookieCutter, user__username=username, name=cookie)
+    if request.method == 'POST':
+        options = dict(request.POST)
+        tasks.exec_cookiecutter(cookie, request.user.id, options)
+        return redirect('/success')
+
+    return render(request, 'cookiecutters/bake.html', {'cookie': cookie})
+
 
 
 class CookieGeneratorView(DetailView):
