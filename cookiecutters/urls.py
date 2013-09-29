18b2f4783f47f201474a8c8bc8c836f381aa9912
Patrick Arminio <patrick.arminio@gmail.com>
2013-09-29 16:30:02 +0200

    Added some links to the API

--- cookiecutters/urls.py
+++ cookiecutters/urls.py
@@ -5,6 +5,6 @@ from cookiecutters import views
 urlpatterns = patterns('',
     url(r'cookies/$', views.CookieListView.as_view(), name='cookie_list'),
     url(r'status/(?P<task_id>.+)$', views.BakingStatusView.as_view(), name='task_status'),
-    url(r'(?P<username>\w+)/(?P<cookie>\w+)/bake/$', views.BakeCookieView.as_view()),
+    url(r'(?P<username>\w+)/(?P<cookie>\w+)/bake/$', views.BakeCookieView.as_view(), name='bake_cookie'),
     url(r'(?P<username>\w+)/(?P<cookie>\w+)/$', views.JSONBakeView.as_view()),
 )
