495ab3ade6ed09e62e96bc7444f421d82a95fcb5
Germano Gabbianelli <tyrion.mx@gmail.com>
2013-10-03 23:08:33 +0000

    Two minor corrections to bin/setup.

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get -qq update
+apt-get -qq install python2.7
 apt-get -qq install postgresql libpq-dev
 apt-get -qq install rabbitmq-server
 apt-get -qq install python-virtualenv
@@ -15,7 +16,7 @@ function sql {
     postgres "psql postgres -tAc \"$1\""
 }
 
-sql "SELECT 1 FROM pg_roles WHERE rolname='vagrant'" | grep -q 1 || 
+sql "SELECT 1 FROM pg_roles WHERE rolname='vagrant'" | grep -q 1 ||
     postgres createuser -DRS vagrant
 
 sql "SELECT 1 FROM pg_database WHERE datname='dash'" | grep -q 1 ||
