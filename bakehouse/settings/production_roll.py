9384f2eeddf1809265493b4f29302e057d033a24
Patrick Arminio <patrick.arminio@gmail.com>
2013-10-21 14:18:53 +0200

    Readded logging

--- bakehouse/settings/production_roll.py
+++ bakehouse/settings/production_roll.py
@@ -44,3 +44,38 @@ SECRET_KEY = get_env_setting('SECRET_KEY')
 # STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 # STATIC_ROOT = ''
 # STATIC_URL = 'http://static.tyrion.mx/'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        # Include the default Django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        'mail_admins': {
+            'class': 'django.utils.log.AdminEmailHandler',
+            'level': 'ERROR',
+             # But the emails are plain text by default - HTML is nicer
+            'include_html': True,
+        },
+        # Log to a text file that can be rotated by logrotate
+        'logfile': {
+            'class': 'logging.handlers.WatchedFileHandler',
+            #'filename': '/var/log/django/myapp.log'
+            'filename': join(SITE_ROOT, 'logs', 'django.log')
+        },
+    },
+    'loggers': {
+        # Again, default Django configuration to email unhandled exceptions
+        'django.request': {
+            'handlers': ['mail_admins'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+        # Might as well log any errors anywhere else in Django
+        'django': {
+            'handlers': ['logfile'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
